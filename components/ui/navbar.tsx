172ed7d063effb154d014a0b492eb43896b228b0
smhasanjamil <smhasanjamil23@gmail.com>
2025-04-21 02:33:39 +0600

    added new get started button to navbar

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -7,6 +7,15 @@ import { currentUser, logout } from "@/redux/features/auth/authSlice";
 import { usePathname, useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { FaArrowRightLong } from "react-icons/fa6";
+import { CgLogIn } from "react-icons/cg";
+import { MdOutlineAppRegistration } from "react-icons/md";
 
 const Navbar = () => {
   const user = useAppSelector(currentUser);
@@ -90,6 +99,29 @@ const Navbar = () => {
                 </div>
               );
             })}
+
+            <DropdownMenu>
+              <DropdownMenuTrigger className="text-feed-black border-feed-black hover:bg-feed-lime flex h-10 cursor-pointer items-center gap-2 rounded-full border-[1.9px] bg-transparent px-2 text-lg font-medium caret-neutral-50 outline-0 duration-300">
+                Get Started <FaArrowRightLong />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="border-feed-black w-fit space-y-1 rounded-2xl border">
+                <DropdownMenuItem className="bg-feed-lime/50 hover:!bg-feed-jungle group w-ful h-9 rounded-[11px] text-lg font-medium duration-300">
+                  <CgLogIn className="text-feed-jungle group-hover:text-feed-lime size-5.5" />
+                  <Link
+                    href="/login"
+                    className="text-feed-jungle group-hover:text-feed-lime"
+                  >
+                    Sign In
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="bg-feed-jungle hover:!bg-feed-black w-ful h-9 rounded-[11px] text-lg font-medium duration-300">
+                  <MdOutlineAppRegistration className="text-feed-lime size-5.5" />
+                  <Link href="/register" className="text-feed-lime">
+                    Sign Up
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
 
             {user ? (
               <>
