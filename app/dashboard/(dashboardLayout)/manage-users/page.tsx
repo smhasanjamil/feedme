46bbbf79d4f236930cfb9b107ae95a83c557dd42
smhasanjamil <smhasanjamil23@gmail.com>
2025-04-22 22:10:23 +0600

    Implement user management functionality with API integration using redux

--- app/dashboard/(dashboardLayout)/manage-users/page.tsx
+++ app/dashboard/(dashboardLayout)/manage-users/page.tsx
@@ -2,7 +2,8 @@ import UserManagementTable from '@/components/userManagement';
 
 const page = () => {
     return (
-        <div>
+        <div className="container mx-auto px-10 py-2">
+            <h3 className='text-2xl font-bold'>User Management</h3>
             <UserManagementTable/>
         </div>
     );
--- components/userManagement/index.tsx
+++ components/userManagement/index.tsx
@@ -1,104 +1,35 @@
-import { columns, DashboardUserData } from "./columns"
-import { DataTable } from "./data-table"
+"use client";
+import { useGetAllUsersQuery } from "@/redux/features/user/userApi";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
+export default function UserManagementTable() {
+  //   const data = await getData()
 
-async function getData(): Promise<DashboardUserData[]> {
-  // Fetch data from your API here.
-  return [
-    {
-        name: "Hasan",
-        _id: "68078498b84b041d6297fa01",
-        email: "hasan@gmail.com",
-        role: "user",
-        isBlocked: false,
-      },
-      {
-        name: "Jamil",
-        _id: "68078498b84b041d6297fa02",
-        email: "jamil@gmail.com",
-        role: "user",
-        isBlocked: false,
-      },
-      {
-        name: "Rafiq",
-        _id: "68078498b84b041d6297fa03",
-        email: "rafiq@gmail.com",
-        role: "admin",
-        isBlocked: false,
-      },
-      {
-        name: "Karim",
-        _id: "68078498b84b041d6297fa04",
-        email: "karim@gmail.com",
-        role: "user",
-        isBlocked: true,
-      },
-      {
-        name: "Nasir",
-        _id: "68078498b84b041d6297fa05",
-        email: "nasir@gmail.com",
-        role: "user",
-        isBlocked: false,
-      },
-      {
-        name: "Sabbir",
-        _id: "68078498b84b041d6297fa06",
-        email: "sabbir@gmail.com",
-        role: "admin",
-        isBlocked: false,
-      },
-      {
-        name: "Tariq",
-        _id: "68078498b84b041d6297fa07",
-        email: "tariq@gmail.com",
-        role: "user",
-        isBlocked: false,
-      },
-      {
-        name: "Mahmud",
-        _id: "68078498b84b041d6297fa08",
-        email: "mahmud@gmail.com",
-        role: "user",
-        isBlocked: true,
-      },
-      {
-        name: "Nayeem",
-        _id: "68078498b84b041d6297fa09",
-        email: "nayeem@gmail.com",
-        role: "admin",
-        isBlocked: false,
-      },
-      {
-        name: "Shakib",
-        _id: "68078498b84b041d6297fa0a",
-        email: "shakib@gmail.com",
-        role: "user",
-        isBlocked: false,
-      },
-      {
-        name: "Ehsan",
-        _id: "68078498b84b041d6297fa0b",
-        email: "ehsan@gmail.com",
-        role: "user",
-        isBlocked: false,
-      },
-      {
-        name: "Mamun",
-        _id: "68078498b84b041d6297fa0c",
-        email: "mamun@gmail.com",
-        role: "admin",
-        isBlocked: true,
-      },
-    // ...
-  ]
-}
+  const { data, isLoading, isError } = useGetAllUsersQuery();
 
-export default async function UserManagementTable() {
-  const data = await getData()
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error fetching data. Please try again later.</div>;
+  }
+
+  const filteredData =
+    data?.map((user) => ({
+      name: user.name,
+
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+      isBlocked: user.isBlocked,
+    })) || [];
+
 
   return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+    <div>
+      <DataTable columns={columns} data={filteredData} />
     </div>
-  )
+  );
 }
